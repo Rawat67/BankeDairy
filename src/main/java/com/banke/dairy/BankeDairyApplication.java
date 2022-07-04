0cd11d02e2d7cd434a63cdc7e700c458cd27f40a
Amit <rawatamit31996@gmail.com>
2022-07-04 23:27:42 +0530

    First commit

---
+++ src/main/java/com/banke/dairy/BankeDairyApplication.java
@@ -0,0 +1,13 @@
+package com.banke.dairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankeDairyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankeDairyApplication.class, args);
+	}
+
+}
