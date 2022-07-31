dbfb8c2a95cbb61127602ea6e0a1fc1235ade711
rawat <rawat@Rawat67>
2022-07-31 23:56:23 +0530

    Added logging and db properties

---
+++ src/main/java/com/banke/dairy/entity/Admin.java
@@ -0,0 +1,55 @@
+package com.banke.dairy.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="admin")
+public class Admin {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer adminId;
+	private String name;
+	private String emailId;
+	private String phone;
+	private String password;
+	public Integer getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(Integer adminId) {
+		this.adminId = adminId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Admin [adminId=" + adminId + ", name=" + name + ", emailId=" + emailId + ", phone=" + phone
+				+ ", password=" + password + "]";
+	}
+	
+}
